c3c22a598989b41e469dbfd3751ffa5e6019b1ee
Nav <navdeep.dhamrait@thebioagency.com>
2023-11-02 12:27:28 -0400

    fixed image issue

--- client/src/Views/MatchUp.jsx
+++ client/src/Views/MatchUp.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext,  useState } from 'react';
 import Button from '../Atoms/Button/Button';
 import InputAndButtons from '../Molecules/InputAndButtons/InputAndButtons';
 import Input from '../Atoms/Input/Input';
