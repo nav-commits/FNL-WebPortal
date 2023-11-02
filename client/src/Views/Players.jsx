72e38dfd425237698047fc9c3e4d46cf317272d4
Nav <navdeep.dhamrait@thebioagency.com>
2023-11-02 11:34:46 -0400

    added add player page

--- client/src/Views/Players.jsx
+++ client/src/Views/Players.jsx
@@ -2,14 +2,27 @@ import React from 'react';
 import { fnlPlayers } from '../Utils';
 import Card from '../Molecules/Card/Card';
 import Button from '../Atoms/Button/Button';
+import { useNavigate } from 'react-router-dom';
 
 const Players = () => {
+    const navigate = useNavigate();
+    const onClick = () => {
+        navigate(`/addPlayer`);
+    };
+
     return (
         // will be replaced with players api when created
         <div style={{ margin: '20px' }}>
             <h1>FNL Players</h1>
-            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'flex-end', marginRight: '10px' }}>
-                <Button title='Add Player' color='#0074D9' width={'205px'} />
+            <div
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'flex-end',
+                    marginRight: '10px',
+                }}
+            >
+                <Button title='Add Player' onClick={onClick} color='#0074D9' width={'205px'} />
             </div>
             <div style={{ display: 'flex', flexWrap: 'wrap' }}>
                 <Card fnlPlayers={fnlPlayers} />
