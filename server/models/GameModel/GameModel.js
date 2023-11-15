1d402ba57b7908761420f1d443a65e9037c1573a
Nav <navdeep.dhamrait@thebioagency.com>
2023-11-15 11:22:01 -0500

    added series winnter key

--- server/models/GameModel/GameModel.js
+++ server/models/GameModel/GameModel.js
@@ -1,22 +1,22 @@
 const mongoose = require('mongoose');
 const gameSchema = new mongoose.Schema({
-   
-        teamWhite: {
-            Team: String,
-            players: [{}],
-            goalie: String,
-        },
-        teamBlack: {
-            Team: String,
-            players: [{}],
-            goalie: String,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now, // Set the default value to the current date and time
-        },
-    
+    teamWhite: {
+        Team: String,
+        players: [{}],
+        goalie: String,
+    },
+    teamBlack: {
+        Team: String,
+        players: [{}],
+        goalie: String,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now, // Set the default value to the current date and time
+    },
+    teamSeriesWinner: {
+        type: String,
+    },
 });
-
 
 module.exports = mongoose.model('Game', gameSchema);
